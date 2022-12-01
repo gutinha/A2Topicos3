e44a0db7275c39ce791d3c223dc7da00ce7d1749
Gutinha <mendesxavier@unitins.br>
2022-12-01 16:47:25 -0300

    Improve login page, hash for password and some stuff

--- A2Topicos3/Controllers/HomeController.cs
+++ A2Topicos3/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using A2Topicos3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace A2Topicos3.Controllers
@@ -7,6 +8,7 @@ namespace A2Topicos3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        TpContext db = new TpContext();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -21,6 +23,30 @@ namespace A2Topicos3.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Login")]
+        public IActionResult Login(string email, string senha)
+        {
+            var user = db.Usuarios.Where(u => u.Email == email && u.Senha == Util.hash(email+senha)).FirstOrDefault();
+            if (user != null)
+            {
+                HttpContext.Session.SetInt32("id_user", user.Id);
+                HttpContext.Session.SetString("nome_user", user.Nome);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
